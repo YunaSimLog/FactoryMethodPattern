9f8922e3a4195b795c5fa670e82987e4c2e39072
yunasim <ynsim@solidit.co.kr>
2023-06-03 01:14:18 +0900

    #✨feature: 팩토리 메소드 클래스 활용 코드 작성.

--- FactoryMethodPattern/FactoryMethodPattern/Program.cs
+++ FactoryMethodPattern/FactoryMethodPattern/Program.cs
@@ -10,6 +10,20 @@ namespace FactoryMethodPattern
     {
         static void Main(string[] args)
         {
+            AnimalCreator[] animalCreators = new AnimalCreator[3];
+            AnimalCreator animalCreatorCow = new CowCreator();
+            AnimalCreator animalCreatorDog = new DogCreator();
+            AnimalCreator animalCreatorChicken = new ChickenCreator();
+
+            animalCreators[0] = animalCreatorCow;
+            animalCreators[1] = animalCreatorDog;
+            animalCreators[2] = animalCreatorChicken;
+
+            foreach (var animalCreator in animalCreators)
+            {
+                var animal = animalCreator.Create();
+                Console.WriteLine(animal.Cry());
+            }
         }
     }
 }
